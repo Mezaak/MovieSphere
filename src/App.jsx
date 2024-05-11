ccfdc7e666b279f8d7197299758737ead4a83c6b
Mezaak <brnkky@gmail.com>
2024-05-12 01:45:51 +0300

    designing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Details from './components/details/Details';
+import Home from './components/home/Home';
 
-function App() {
-  
-
-  return (
-    <>
-      <div>Hello</div>
-    </>
-  )
-}
-
+const App = () =>{
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/movie:id" element={<Details/>} />
+    </Routes>
+    
+    
+    </BrowserRouter>
+  );
+};
 export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1 className='heading'>Movie Search App</h1>
+    </div>
+  )
+}
+
+export default Header
