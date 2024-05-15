285644f2965510433b0aae91d7914e5114760af9
Mezaak <brnkky@gmail.com>
2024-05-16 01:25:39 +0300

    adjusting files

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import {Provider} from 'react-redux'
+import store from './redux/store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
